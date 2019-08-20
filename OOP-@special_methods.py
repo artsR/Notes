77d05634c8bec359adabe9319035488c4b28486f
artsR <asihdi@gmail.com>
2019-08-20 13:40:12 +0300

    add some notes after watched 'buildwithpython'

--- OOP-@special_methods.py
+++ OOP-@special_methods.py
@@ -2,7 +2,8 @@
 class Animal():
     animals = [] # every child has access to this list.
 
-    def __init__(self, name, age):
+    def __init__(self, name, age): # I may change name of variable within 'Animal'
+                                # and I don't have to change it in 'child class'.
         self.name = name
         self.age  = age
         self.animals.append(self)
@@ -19,9 +20,11 @@ class Animal():
         return len(cls.animals)
 
 class Dog(Animal):
-    def __init__(self, name='', age=0):
-        super().__init__(name, age)
+    num_dogs = 0
+    def __init__(self, name='', age=0): # I don't even have to use 'super()'. I may input 'pass'
+        super().__init__(name, age)     # and it still will be working. However I want to add another variable: 'type' so I need it.
         self.type   = 'dog'
+        Dog.num_dogs += 1
     @staticmethod # doesn't have access to class variables and instances
     def talk(n):
         for _ in range(n):
@@ -39,5 +42,16 @@ print(Animal.animals[1].speak())
 print(daisy.num_animals())
 
 daisy.talk(2)
-Animal.talk() #it's error because there is not 'self' (instance like ex. 'daisy')
+#Animal.talk() #it's error because there is not 'self' (instance like ex. 'daisy')
 Dog.talk(3)   #it's not error because 'talk' in 'Dog class' doesn't require 'self' arg.
+print(Dog.num_dogs)
+simba = Dog('simba')
+print(simba.num_dogs)
+
+# Check if 'instance A' belongs to 'class X':
+isinstance(daisy, Dog)      # True
+isinstance(daisy, Animal)   # True
+
+issubclass(Dog, Dog)        # True
+issubclass(Dog, Animal)     # True
+issubclass(Animal, Dog)     # False
--- OOP-private_public.py
+++ OOP-private_public.py
@@ -1,4 +1,2 @@
-"""
-Generally speaking w Python doesn't exist division of public/private classes.
-In other languages 'private methods' can be called only from within the class.
-"""
+"""Generally speaking w Python doesn't exist division of public/private classes.
+In other languages 'private methods' can be called only from within the class."""
